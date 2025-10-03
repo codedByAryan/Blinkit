12dcc1d0cf74ac2a04ff356cc8f17311c07d135d
Aryan@3001 <aryanchauhan9424@gmail.com>
2025-10-03 19:26:43 +0530

    Create client side

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='border-t'>
+        <div className = 'container mx-auto p-4'>
+
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
